287eba8a0ad73d25e46cb5c83eeb639742457c44
Thomas Segismont <tsegismo@redhat.com>
2025-03-19 15:12:42 +0100

    Implement dummy resource

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,8 @@
 package org.acme;
 
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.sqlclient.Pool;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +11,13 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    Pool pool;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public Uni<String> hello() {
+        return pool.query("SELECT 1").execute()
+                .onItem().transform(rows -> "Hello");
     }
 }
